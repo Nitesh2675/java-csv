a3176f5e297aff69973150df246554fca073d5d4
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-04-07 14:37:41 +0530

    [Nitesh] ADD: read and count

---
+++ src/main/java/com/bridgelabz/ReadAndCount.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadAndCount {
+    public static void main(String[] args) {
+        try(BufferedReader br=new BufferedReader(new FileReader("test.csv"))){
+            String line;
+            int c=0;
+            while((line= br.readLine())!=null){
+                c++;
+            }
+            System.out.println(c-1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
